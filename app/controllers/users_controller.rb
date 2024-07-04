fe4e19d01305f16166407c13beb69325cdbe84fa
stevechan <chanshunli@gmail.com>
2024-07-04 20:37:03 +0800

    老的文件：https://github.com/zieeco/rails-blog-app/blob/development/app/controllers/users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,10 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, except: [:login]
+  before_action :authenticate_user!
   def index
     @users = User.all
   end
 
   def show
     @user = User.find_by(id: params[:id])
-  end
-
-  def login
-    phone_number = params[:phone_number]
-    if valid_phone_number?(phone_number)
-      @user = User.find_by(phone_number: phone_number)
-      if @user
-        sign_in(@user)
-        render json: { message: "Login successful" }
-      else
-        render json: { error: "User not found" }, status: :not_found
-      end
-    else
-      render json: { error: "Invalid phone number" }, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def valid_phone_number?(phone_number)
-    # implement phone number validation logic here
-    # return true if phone number is valid, false otherwise
-  end
-
-  def sign_in(user)
-    # implement sign in logic here
   end
 end
